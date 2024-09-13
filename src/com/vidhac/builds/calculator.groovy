3427f3264fde24f6b73fed4883ba029c416f44ab
itsvidha <gcpdevops0924@gmail.com>
2024-09-13 12:13:32 +0530

    made changes

---
+++ src/com/vidhac/builds/calculator.groovy
@@ -0,0 +1,16 @@
+package com.vidhac.builds;
+class Calculator{
+    def jenkins
+    Calculator(jenkins)
+    {
+        this.jenkins= jenkins
+    }
+    def add(firstNumber, secondNumber)
+    {
+        return firstNumber+secondNumber
+    }
+    def mul(firstNumber, secondNumber)
+    {
+        return firstNumber*secondNumber
+    }
+} 
---
+++ var/firstpipeline.groovy
@@ -0,0 +1,25 @@
+import com.vidhac.builds.Calculator;
+Calculator calculator=new Calculator(this)
+pipeline {
+    agent any 
+    stages{
+        stage('AdditionStage'){
+            steps{
+                echo("printing sum of 2 numbers")
+                println calculator.add(10,20)
+            }
+        }
+        stage('MultiplyingStage'){
+            steps{
+                echo("printing multiplication of 2 numbers")
+                println calculator.mul(10,20)
+            }
+        }
+        stage('mailing'){
+            steps{
+                echo("mail has been sent")
+
+            }
+        }
+    }
+}
