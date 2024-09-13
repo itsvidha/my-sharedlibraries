090e773035f9e1cd34379c5ea0805001d7c7d688
itsvidha <gcpdevops0924@gmail.com>
2024-09-13 12:29:44 +0530

    new

--- var/firstpipeline.groovy
+++ var/firstpipeline.groovy
@@ -1,28 +1,30 @@
 import com.vidhac.builds.Calculator;
 Calculator calculator=new Calculator(this)
-pipeline {
-    agent any 
-    environmet{
-        appname= ${pipelineParams.appname}
-    }
-    stages{
-        stage('AdditionStage'){
-            steps{
-                echo("printing sum of 2 numbers")
-                println calculator.add(10,20)
-                echo "this is ${appname}"
-            }
+def call(Map pipelineParams){
+        pipeline {
+        agent any 
+        environmet{
+            appname= ${pipelineParams.appname}
         }
-        stage('MultiplyingStage'){
-            steps{
-                echo("printing multiplication of 2 numbers")
-                println calculator.mul(10,20)
+        stages{
+            stage('AdditionStage'){
+                steps{
+                    echo("printing sum of 2 numbers")
+                    println calculator.add(10,20)
+                    echo "this is ${appname}"
+                }
             }
-        }
-        stage('mailing'){
-            steps{
-                echo("mail has been sent")
+            stage('MultiplyingStage'){
+                steps{
+                    echo("printing multiplication of 2 numbers")
+                    println calculator.mul(10,20)
+                }
+            }
+            stage('mailing'){
+                steps{
+                    echo("mail has been sent")
 
+                }
             }
         }
     }
